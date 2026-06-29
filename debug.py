594fa736697a7d564f421cc80b18f39b4b861be6
Guy Matz <gmatz@matz.org>
2026-06-29 18:15:07 +0100

    added debug code . . . for debugging

---
+++ debug.py
@@ -0,0 +1,13 @@
+# See: https://www.youtube.com/watch?v=T9vYgZJCmeI
+import pygame
+pygame.init()
+font = pygame.font.Font(None,30)
+
+def debug(info, y = 10, x = 10):
+    print(info)
+    display_surf = pygame.display.get_surface()
+    debug_surf = font.render(str(info),True,'white')
+    debug_rect = debug_surf.get_rect(topleft = (x,y))
+    pygame.draw.rect(display_surf, 'Black',debug_rect)
+    display_surf.blit(debug_surf,debug_rect)
+
