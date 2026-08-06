d57e479a0bb0c8b35da486e23e79d0c8274fcbf0
Guy Matz <gmatz@matz.org>
2026-08-06 15:32:05 -0400

    not sure

--- example.py
+++ example.py
@@ -17,7 +17,7 @@ import time
 import pygame
 from debug import debug
 import colors as c
-#import sys
+import sys
 
 pygame.init()
 clock = pygame.time.Clock()
@@ -49,9 +49,6 @@ red_img = pygame.transform.scale(
     red_img, (red_img.get_width() * 2, red_img.get_height() * 2)
 )
 
-
-# pygame.draw.rect(background, (0, 255, 255), (20, 20, 20 , 20))
-# pygame.draw.rect(background, (255,0,  255), (120, 120, 50 , 50))
 screen.blit(background, (0, 0))
 screen.fill(c.WHITE)
 pygame.display.update()
@@ -66,13 +63,12 @@ moving = False
 # sound = pygame.mixer.Sound("clank.wav")
 
 cycle = 0
-FPS = 30
+FPS = 8
 
 OOPS = False
 
 while running:
 
-    clock.tick(FPS)
     # Comute delta time
     now = time.time_ns()
     dt = now - prev_time
@@ -85,34 +81,25 @@ while running:
         for col in range(0, int(WIDTH / SWIDTH)):
             if (x + row + col) % 2 == 0:
                 # print(f"EVEN:  {x} {r} {c}")
-                screen.blit(green_img, (row * SHEIGHT, col * SWIDTH))
-                screen.blit(red_img, (row * SHEIGHT, (col + 1) * SWIDTH))
+                # screen.blit(green_img, (row * SHEIGHT, col * SWIDTH))
+                # screen.blit(red_img, (row * SHEIGHT, (col + 1) * SWIDTH))
+                pygame.draw.rect(screen, c.DARK_GREEN, pygame.Rect(row * SHEIGHT, col * SWIDTH,
+                                                             SHEIGHT, SWIDTH))
+                pygame.draw.rect(screen, c.RED, pygame.Rect(row * SHEIGHT, (col + 1) * SWIDTH,
+                                                             SHEIGHT, SWIDTH))
             else:
                 # print(f"ODD:  {x} {r} {c}")
-                screen.blit(red_img, (row * SHEIGHT, col * SWIDTH))
-                screen.blit(green_img, (row * SHEIGHT, (col + 1) * SWIDTH))
+                # screen.blit(red_img, (row * SHEIGHT, col * SWIDTH))
+                # screen.blit(green_img, (row * SHEIGHT, (col + 1) * SWIDTH))
+                pygame.draw.rect(screen, c.RED, pygame.Rect(row * SHEIGHT, col * SWIDTH,
+                                                             SHEIGHT, SWIDTH))
+                pygame.draw.rect(screen, c.DARK_GREEN, pygame.Rect(row * SHEIGHT, (col + 1) * SWIDTH,
+                                                             SHEIGHT, SWIDTH))
 
     pygame.draw.rect(screen, c.YELLOW, (Yelx+2, Yely+2, SHEIGHT - 4, SWIDTH - 4))
     cycle = 0
     x = 1 if x == 0 else 0
 
-    #    screen.blit(potato_img, (x, 30))
-    #
-    #    hitbox = pygame.Rect(x, 30, potato_img.get_width(), potato_img.get_height())
-    #
-    #    mpos = pygame.mouse.get_pos()
-    #
-    #    target = pygame.Rect(300, 0, 160, 280)
-    #    collision = hitbox.colliderect(target)
-    #    m_collision = target.collidepoint(mpos)
-    #    pygame.draw.rect(screen, (255 * collision, 255 * m_collision, 0), target)
-    #
-    #    if moving:
-    #        x += 50 * delta_time
-    #
-    #    text = font.render('Hello World!', True, (0, 0, 0))
-    #    screen.blit(text, (300, 100))
-    #
     for event in pygame.event.get():
         print(f"Event: {event}")
         if event.type == pygame.QUIT:
@@ -159,13 +146,14 @@ while running:
     #
     delta_time = clock.tick(60) / 100
     delta_time = max(0.001, min(0.1, delta_time))
-
     cycle = (cycle + 1) % 100
     debug(f"Location: {Yelx}, {Yely}")
-    pygame.display.update()
+
     clock.tick(FPS/8)
+    pygame.display.update()
     # print(cycle)
 
 # sys.exit()
-pygame.display.quit()
+# pygame.display.quit()
 pygame.quit()
+sys.exit()
